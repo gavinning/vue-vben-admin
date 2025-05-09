f4bfe19cb5c70273648d0aa75047451ce1e2a410
gavinning <gavinning@qq.com>
2025-05-09 11:43:22 +0000

    perf: format

--- apps/sky/src/helper/assert.ts
+++ apps/sky/src/helper/assert.ts
@@ -13,6 +13,8 @@ export function ok(condition: any, message: string, notice: boolean = true) {
 
 export function isLikeUUID(str: string) {
   // 这里必须要判断字符串类型，否则数组类型也会返回true
-  return typeof str === 'string' &&
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(str)
+  return (
+    typeof str === 'string' &&
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(str)
+  )
 }
