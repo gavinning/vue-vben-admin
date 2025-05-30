5c12c5f468cb838c558db6f5a2e00a654ad921cf
gavinning <gavinning@qq.com>
2025-05-30 16:31:54 +0800

    0.1.6 添加上传图片宽高限制

--- apps/sky/src/components/PageTable/interceptor/Hooks/files/file.hook.ts
+++ apps/sky/src/components/PageTable/interceptor/Hooks/files/file.hook.ts
@@ -15,34 +15,53 @@ export const FormSchemaProCoding = () => {
 function limitMaxSize(ctx: Context, schema: Schema) {
   const isFile = schema.component === 'Upload'
   const maxSize = schema.componentProps?.maxSize
+  const maxWidth = schema.componentProps?.maxWidth
+  const maxHeight = schema.componentProps?.maxHeight
 
-  if (!isFile || !maxSize) return
+  if (!isFile || !maxSize || !maxWidth || !maxHeight) return
+
+  // 清理不符合条件的文件
+  const resetFormValue = async (file: UploadFile) => {
+    const row = await ctx.api.getValues()
+    const field = row[schema.fieldName]
+
+    if (Array.isArray(field)) {
+      ctx.api.setFieldValue(
+        schema.fieldName,
+        field.filter((i) => i.uid !== file.uid),
+      )
+    } else {
+      ctx.api.setFieldValue(schema.fieldName, null)
+    }
+  }
 
   // 添加onChange事件
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   schema.componentProps!.onChange = async (file: UploadFile) => {
     debug.log('onChange:', file)
-    if (
-      file?.raw &&
-      schema.componentProps?.maxSize &&
-      file.raw.size / 1024 > schema.componentProps.maxSize
-    ) {
+
+    // 限制图片大小
+    if (file?.raw && maxSize && file.raw.size / 1024 > maxSize) {
       Notice.error({
         position: 'bottom-right',
-        message: `图片大小不能超过${schema.componentProps.maxSize}KB!`,
+        message: `图片大小不能超过${maxSize}KB`,
       })
+      resetFormValue(file)
+    }
 
-      const row = await ctx.api.getValues()
-      const field = row[schema.fieldName]
-
-      if (Array.isArray(field)) {
-        ctx.api.setFieldValue(
-          schema.fieldName,
-          field.filter((i) => i.uid !== file.uid),
-        )
-      } else {
-        ctx.api.setFieldValue(schema.fieldName, null)
-      }
+    // 限制图片尺寸
+    if (file?.raw && maxWidth && maxHeight) {
+      const img = new Image()
+      img.src = URL.createObjectURL(file.raw)
+      img.addEventListener('load', () => {
+        if (img.width > maxWidth || img.height > maxHeight) {
+          Notice.error({
+            position: 'bottom-right',
+            message: `图片宽高不能超过${maxWidth}*${maxHeight}`,
+          })
+          resetFormValue(file)
+        }
+      })
     }
   }
 }
